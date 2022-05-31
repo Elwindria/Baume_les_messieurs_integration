60c13ae9142d046f14a89be68071b0d30e3c5d31
Elwindria <p.lopez2000@laposte.net>
2022-05-31 11:55:59 +0200

    Ca marche ! Ajout de class 'activate' via javascript au click

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/* Carousel_discover version Desktop */
+
+// const activate = document.querySelector(".activate");
+const photo_carousel = document.querySelectorAll(".photo_carousel");
+const carousel_1 = document.querySelector(".carousel_1");
+const carousel_2 = document.querySelector(".carousel_2");
+const carousel_3 = document.querySelector(".carousel_3");
+const carousel_4 = document.querySelector(".carousel_4");
+
+carousel_1.onclick = function() {
+    remove_activate();
+    carousel_1.classList.add("activate");
+}
+
+carousel_2.onclick = function() {
+    remove_activate();
+    carousel_2.classList.add("activate");
+}
+
+carousel_3.onclick = function() {
+    remove_activate();
+    carousel_3.classList.add("activate");
+}
+
+carousel_4.onclick = function() {
+    remove_activate();
+    carousel_4.classList.add("activate");
+    
+}
+
+function remove_activate() {
+    photo_carousel.forEach(element => {
+        element.classList.remove("activate");
+    });
+}
