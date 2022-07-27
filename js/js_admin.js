60f741c3cfe071d4aed2662b7d38034113e35305
Elwindria <p.lopez2000@laposte.net>
2022-07-27 12:01:08 +0200

    Ajout des img poubelle en css + changement leger dans les hover/animation + taille

--- js/js_admin.js
+++ js/js_admin.js
@@ -6,27 +6,34 @@ function ListMail(){
     fetch ('php/listMail.php')
     .then(response => response.json())
     .then((result) => {
-        const tbodyTable = document.querySelector('tbody');
+        createTable(result);
+    })
+}
+
+function createTable(result){
+
+    const tbodyTable = document.querySelector('tbody');
         
-        result.forEach( mail => { 
-            
-            let newRowElement = document.createElement('tr');
-            tbodyTable.appendChild(newRowElement);
-            
-            let newColumn = document.createElement('td'); 
-            newRowElement.appendChild(newColumn);
-            let newValueColumn = document.createTextNode(mail['date_mail']);
-            newColumn.appendChild(newValueColumn);
-            
-            let newColumn2 = document.createElement('td'); 
-            newRowElement.appendChild(newColumn2);
-            let newValueColumn2 = document.createTextNode(mail['mail']);
-            newColumn2.appendChild(newValueColumn2);
-            
-            let newColumn3 = document.createElement('td'); 
-            newRowElement.appendChild(newColumn3);
-            let newValueColumn3 = document.createTextNode('poubelle');
-            newColumn3.appendChild(newValueColumn3);
-        })
+    result.forEach( mail => { 
+        
+        let newRowElement = document.createElement('tr');
+        tbodyTable.appendChild(newRowElement);
+        
+        let newColumn = document.createElement('td'); 
+        newRowElement.appendChild(newColumn);
+        let newValueColumn = document.createTextNode(mail['date_mail']);
+        newColumn.appendChild(newValueColumn);
+        
+        let newColumn2 = document.createElement('td'); 
+        newRowElement.appendChild(newColumn2);
+        let newValueColumn2 = document.createTextNode(mail['mail']);
+        newColumn2.appendChild(newValueColumn2);
+        
+        let newColumn3 = document.createElement('td'); 
+        newRowElement.appendChild(newColumn3);
+        let newValueColumn3 = document.createElement("img");
+        newValueColumn3.src = "images/trash.png";
+        newValueColumn3.classList.add("trash");
+        newColumn3.appendChild(newValueColumn3);
     })
 }
